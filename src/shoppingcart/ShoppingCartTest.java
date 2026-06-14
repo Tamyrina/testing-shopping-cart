e519591774d71b35b8cb581c9985abafa4fd1b4f
Vanessa <vanessa_@gmx.net>
2026-06-14 13:02:20 +0200

    red: add failing test for empty shopping cart

--- src/shoppingcart/ShoppingCartTest.java
+++ src/shoppingcart/ShoppingCartTest.java
@@ -1,5 +1,16 @@
 package shoppingcart;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class ShoppingCartTest {
 
+    @Test
+    void emptyCartHasTotalPriceZero() {
+        ShoppingCart cart = new ShoppingCart();
+
+        double total = cart.getTotalPrice();
+
+        assertEquals(0.0, total);
+    }
 }
